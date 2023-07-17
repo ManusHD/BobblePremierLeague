34e1a262d2b5c128e7d02a7c958869afb14d7df7
Manu <mhorrillj@alumnos.unex.es>
2023-07-17 19:37:31 +0200

    solo t3

--- scripts/estadisticas.js
+++ scripts/estadisticas.js
@@ -1,6 +1,6 @@
 const API_KEY = 'AIzaSyD1qXjPmgBaRtX0zJtH76nvU708Gvs3A-g';
 
-fetch('https://sheets.googleapis.com/v4/spreadsheets/1zaCd8es0pm2md9X8AJwJRm-0J22_7UxoRNAztGzpkmk/values/Estadisticas!A4:P?key=AIzaSyD1qXjPmgBaRtX0zJtH76nvU708Gvs3A-g')
+fetch('https://sheets.googleapis.com/v4/spreadsheets/1zaCd8es0pm2md9X8AJwJRm-0J22_7UxoRNAztGzpkmk/values/Estadisticas!A4:P33?key=AIzaSyD1qXjPmgBaRtX0zJtH76nvU708Gvs3A-g')
 .then(response => response.json())
   .then(data => {
     const values = data.values;
