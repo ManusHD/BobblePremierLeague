fd0659c8fe8580bdc2af31595b01742d0d9f3469
JoseN88 <134077876+JoseN88@users.noreply.github.com>
2023-07-21 19:51:13 +0200

    Update partidosLiberation.js
    Para que se vean los partidos de cuartos

--- scripts/partidosLiberation.js
+++ scripts/partidosLiberation.js
@@ -8,7 +8,7 @@ const dias = [
   'Sábado',
 ];
 
-fetch('https://sheets.googleapis.com/v4/spreadsheets/1zaCd8es0pm2md9X8AJwJRm-0J22_7UxoRNAztGzpkmk/values/Partidos!P2:T7?key=AIzaSyD1qXjPmgBaRtX0zJtH76nvU708Gvs3A-g')
+fetch('https://sheets.googleapis.com/v4/spreadsheets/1zaCd8es0pm2md9X8AJwJRm-0J22_7UxoRNAztGzpkmk/values/Partidos!P2:T11?key=AIzaSyD1qXjPmgBaRtX0zJtH76nvU708Gvs3A-g')
 .then(response => response.json())
   .then(data => {
     const matches = data.values;
