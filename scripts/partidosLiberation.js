69066c48a4f396ccfdcc4e310a7c85430e1f6164
JoseN88 <134077876+JoseN88@users.noreply.github.com>
2023-07-31 18:21:39 +0200

    Update partidosLiberation.js

--- scripts/partidosLiberation.js
+++ scripts/partidosLiberation.js
@@ -8,7 +8,7 @@ const dias = [
   'Sábado',
 ];
 
-fetch('https://sheets.googleapis.com/v4/spreadsheets/1zaCd8es0pm2md9X8AJwJRm-0J22_7UxoRNAztGzpkmk/values/Partidos!P2:T13?key=AIzaSyD1qXjPmgBaRtX0zJtH76nvU708Gvs3A-g')
+fetch('https://sheets.googleapis.com/v4/spreadsheets/1zaCd8es0pm2md9X8AJwJRm-0J22_7UxoRNAztGzpkmk/values/Partidos!P2:T14?key=AIzaSyD1qXjPmgBaRtX0zJtH76nvU708Gvs3A-g')
 .then(response => response.json())
   .then(data => {
     const matches = data.values;
